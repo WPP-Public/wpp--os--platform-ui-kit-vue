9af01b5d331fa5c8a09f5b8f869def23551fac08
DragosCornean97 <dragos.cornean@wpp.com>
2025-11-06 16:48:39 +0200

    Release v2.22.0

--- lib/vue-component-lib/utils.d.ts
+++ lib/vue-component-lib/utils.d.ts
@@ -1,4 +1,8 @@
 export interface InputProps<T> {
     modelValue?: T;
 }
-export declare const defineContainer: <Props, VModelType = string | number | boolean>(name: string, defineCustomElement: any, componentProps?: string[], modelProp?: string, modelUpdateEvent?: string, externalModelUpdateEvent?: string) => import("vue").DefineSetupFnComponent<Props & InputProps<VModelType>, {}, {}, Props & InputProps<VModelType> & {}, import("vue").PublicProps>;
+export declare const defineContainer: <Props, VModelType = string | number | boolean>(name: string, defineCustomElement: any, componentProps?: string[], modelProp?: string, modelUpdateEvent?: string | undefined, externalModelUpdateEvent?: string | undefined) => import("vue").DefineComponent<Props & InputProps<VModelType>, object, {}, import("vue").ComputedOptions, import("vue").MethodOptions, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<Props & InputProps<VModelType> extends import("vue").ComponentPropsOptions<{
+    [x: string]: unknown;
+}> ? import("vue").ExtractPropTypes<import("vue").ComponentPropsOptions<{
+    [x: string]: unknown;
+}> & Props & InputProps<VModelType>> : Props & InputProps<VModelType>>, import("vue").ExtractDefaultPropTypes<Props & InputProps<VModelType>>>;
